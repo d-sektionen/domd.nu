4af66a2fbc0be27b9d454a9d4e5790462ca515cc
Marcus Döberl <marcus.doberl@gmail.com>
2021-10-14 09:27:49 +0200

    Added error 404

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import styled from "styled-components";
+import Countdown from "react-countdown";
 
-function App() {
+const hundreddays = new Date("2021-12-31T00:00:00");
+
+const render = ({ days, hours, minutes, seconds }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <span>
+      {" "}
+      {days} days {hours} hours {minutes} minutes and {seconds} seconds left
+    </span>
   );
-}
+};
 
-export default App;
+const Title = styled.h1`
+  font-family: "Courier New", monospace;
+  color: white;
+  font-size: 2rem;
+  text-align: center;
+  margin-top: 10%;
+`;
+
+const Grid = styled.div`
+  background-color: black;
+  height: 100vh;
+  margin-left: auto;
+  
+  
+`;
+
+const Row = styled.div`
+  
+  padding-top: 2vh;
+  
+  
+  
+`;
+export const Col = styled.div`
+  flex: ${(props) => props.size};
+  padding: 2vw;
+`;
+
+export const ÖMSpan = styled.span`
+  font-size: 1.5rem;
+  
+`;
+
+const error404 = () => {
+  return (
+    <>
+      <Grid>
+        <Row>
+          <Title>
+            Error 404: not 100 days left until D<ÖMSpan>ÖM</ÖMSpan>D yet
+          </Title>
+        </Row>
+        <Row>
+          <Title >
+            It is days <Countdown date={hundreddays} renderer={render} /> until
+            100 days left until D<ÖMSpan>ÖM</ÖMSpan>D
+          </Title>
+        </Row>
+      </Grid>
+    </>
+  );
+};
+
+export default error404;
