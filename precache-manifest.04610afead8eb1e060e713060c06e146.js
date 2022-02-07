f7663bb09c617280fcab96cd8e9fe3e3c7dd7c54
Marcus Döberl <marcus.doberl@gmail.com>
2022-02-07 13:57:13 +0100

    Updates

---
+++ precache-manifest.04610afead8eb1e060e713060c06e146.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3c34b6f553e68dc0487aaff11c8aea8",
+    "url": "/domd.nu/index.html"
+  },
+  {
+    "revision": "cd4aec6c449be978b256",
+    "url": "/domd.nu/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d9da6bc3059aea4741ee",
+    "url": "/domd.nu/static/js/2.a4f9d919.chunk.js"
+  },
+  {
+    "revision": "55b87968dd052bfd6ba0f730cd9fc8c8",
+    "url": "/domd.nu/static/js/2.a4f9d919.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cf3a9cd2e192cf7c87d",
+    "url": "/domd.nu/static/js/3.e2b59c79.chunk.js"
+  },
+  {
+    "revision": "cd4aec6c449be978b256",
+    "url": "/domd.nu/static/js/main.cacaf1dd.chunk.js"
+  },
+  {
+    "revision": "00bb4252a3019d3e6c02",
+    "url": "/domd.nu/static/js/runtime-main.e966dce1.js"
+  },
+  {
+    "revision": "e72a9de6c566e7d9342922525cc2c3c0",
+    "url": "/domd.nu/static/media/Cygate.e72a9de6.webp"
+  },
+  {
+    "revision": "baea0123a8be0c432daa69f7b64e5954",
+    "url": "/domd.nu/static/media/D-group.baea0123.png"
+  },
+  {
+    "revision": "e733a00426af7456889e5e09119635ba",
+    "url": "/domd.nu/static/media/dart.e733a004.png"
+  },
+  {
+    "revision": "63263905452eee94b020f558b4bb0d30",
+    "url": "/domd.nu/static/media/xameraSpons.63263905.webp"
+  }
+]);
