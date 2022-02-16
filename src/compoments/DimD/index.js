4a1bf933724794ec1f5bc95fa4c9d716d715e0bf
Marcus Döberl <marcus.doberl@gmail.com>
2022-02-16 11:57:52 +0100

    Added dimd bracket

--- src/compoments/DimD/index.js
+++ src/compoments/DimD/index.js
@@ -2,41 +2,64 @@ import React from "react";
 import Typography from "@mui/material/Typography";
 import dart from "../../res//dart.gif";
 import Grid from "@mui/material/Grid";
-import Box from '@mui/material/Box'
-import { NavLink} from "react-router-dom"
+import Box from "@mui/material/Box";
+import { NavLink } from "react-router-dom";
 
 function DimD() {
   return (
-    <Box sx={{ flexGrow: 1, color:"white", backgroundColor:"black" }}>
-      <img src={dart} alt="dartild" style={{height:"40vh", width:"100%"}}/>
+    <Box sx={{ flexGrow: 1, color: "white", backgroundColor: "black" }}>
+      <img src={dart} alt="dartild" style={{ height: "40vh", width: "100%" }} />
       <Grid container>
-        <Grid sm={12} md={6}  sx={{m:4}}>
-          <Typography variant="h2"  gutterBottom sx={{fontWeight:"bold", textAlign:"center"}}>
+        <Grid xs={12} sx={{ m: { xs: 4, m: 4 } }}>
+          <Typography
+            variant="h4"
+            gutterBottom
+            sx={{ fontWeight: "bold", textAlign: "center" }}
+          >
+            {" "}
+            Bracket D<span tyle="font-size:75%">IM</span>D 2022
+          </Typography>
+          <iframe
+            src="https://challonge.com/dimd2022/module"
+            title="DimD2022"
+            width="100%"
+            height="500"
+            frameborder="0"
+            scrolling="auto"
+            allowtransparency="true"
+          ></iframe>
+        </Grid>
+        <Grid sm={12} md={6} sx={{ m: 4 }}>
+          <Typography
+            variant="h2"
+            gutterBottom
+            sx={{ fontWeight: "bold", textAlign: "center" }}
+          >
             {" "}
             Tidigare vinnare av D<span tyle="font-size:75%">IM</span>D
           </Typography>
           <Typography variant="h4" gutotmButtom color>
-          <ul>
-            <li>2021 - Payam Tavakoli </li>
-            <li>2020 - Theodor Fällman</li>
-            <li>2019 - Theodor Fällman</li>
-            <li>2018 - Marcus Jonsson</li>
-            <li>2017 - Tim Lestander</li>
-            <li>2016 - Sebastian Ragnarsson</li>
-            <li>2015 - Tobias Hultqvist</li>
-          </ul>
+            <ul>
+              <li>2021 - Payam Tavakoli </li>
+              <li>2020 - Theodor Fällman</li>
+              <li>2019 - Theodor Fällman</li>
+              <li>2018 - Marcus Jonsson</li>
+              <li>2017 - Tim Lestander</li>
+              <li>2016 - Sebastian Ragnarsson</li>
+              <li>2015 - Tobias Hultqvist</li>
+            </ul>
           </Typography>
         </Grid>
-        <Grid sm={12} md={4} sx={{textAlign:"center", m:4}}>
-          <Typography variant="h2" gutterBottom sx={{fontWeight:"bold"}}>
+        <Grid sm={12} md={4} sx={{ textAlign: "center", m: 4 }}>
+          <Typography variant="h2" gutterBottom sx={{ fontWeight: "bold" }}>
             {" "}
             Regler
           </Typography>
-          <Typography variant="h5" gutterBottom >
+          <Typography variant="h5" gutterBottom>
             {" "}
             Alla regler för D<span STYLE="font-size:75%">IM</span>D och D
-            <span STYLE="font-size:75%">ÖM</span>D finns på får regel sida som du kan
-            komma till genom att klicka <NavLink to="rules">här</NavLink> 
+            <span STYLE="font-size:75%">ÖM</span>D finns på får regel sida som
+            du kan komma till genom att klicka <NavLink to="rules">här</NavLink>
           </Typography>
         </Grid>
       </Grid>
@@ -45,4 +68,3 @@ function DimD() {
 }
 
 export default DimD;
-
