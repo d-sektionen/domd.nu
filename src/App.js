3a9ead7908d6be82332d21038250196a347f8cf8
Marcus Döberl <marcus.doberl@gmail.com>
2022-04-20 20:02:47 +0200

    back to error404

--- src/App.js
+++ src/App.js
@@ -6,19 +6,20 @@ import Rules from "./compoments/Rules"
 import DimD from "./compoments/DimD";
 import Ulag from "./compoments/U_lag";
 import Lag from "./compoments/Lag"
+import Error404 from "./compoments/error404"
 
 
 import NavbarNew from "./compoments/Navbartest";
 
-
+//<NavbarNew/>
 
 function App() {
 
   return (
     <Router>
-      <NavbarNew/>
+      
       <Routes>
-        <Route exact path="/" element={<StartPage/>} />
+        <Route exact path="/" element={<Error404/>} />
         <Route exact path="/domd.nu" element={<StartPage/>} />
         <Route exact path="/dimd" element={<DimD/>} />
         <Route exact path="/rules" element={<Rules/>} />
--- src/compoments/error404/index.js
+++ src/compoments/error404/index.js
@@ -1,7 +1,11 @@
 
 import Countdown from "react-countdown";
-import {Title, Grid, Row, ÖMSpan} from "./error404elements"
+import {Title, Row, ÖMSpan} from "./error404elements"
 import React from 'react';
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import { styled } from "@mui/material/styles";
 
 const hundreddays = new Date("2021-12-31T00:00:00");
 
@@ -14,23 +18,33 @@ const render = ({ days, hours, minutes, seconds }) => {
   );
 };
 
-const Error404 = () => {
-  return (
-    <>
-      <Grid>
-        <Row>
-          <Title>
-            Error 404: not 100 days left until D<ÖMSpan>ÖM</ÖMSpan>D yet
-          </Title>
-        </Row>
-        <Row>
+const Root = styled(Box)(({ theme }) => ({
+  backgroundRepeat: "no-repeat",
+  backgroundSize: "cover",
+  backgroundAttachment: "fixed",
+  backgroundColor:"black",
+  color: "white",
+  textAlign: "center",
+  width:"100vw",
+  height:"100vh"  
+}));
+/*<Row>
           <Title >
             It is days <Countdown date={hundreddays} renderer={render}></Countdown> until
             100 days left until D<ÖMSpan>ÖM</ÖMSpan>D
           </Title>
+        </Row> */
+const Error404 = () => {
+  return (
+    <Root container>
+      <Grid >
+        <Row>
+          <Typography variant="h4" sx={{color:"white", pt:{md:40, xs:35}}}>
+            Error 404: not 100 days left until D<ÖMSpan>ÖM</ÖMSpan>D yet
+          </Typography>
         </Row>
       </Grid>
-    </>
+    </Root>
   );
 };
 
