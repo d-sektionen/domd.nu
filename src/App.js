edb2aeb5d0936f8591c06b776ab2df2b1206e0d8
Marc Taylor <marc@taylorit.com>
2023-06-27 14:22:49 +0200

    shows error404

--- src/App.js
+++ src/App.js
@@ -15,18 +15,34 @@ import NavbarNew from "./compoments/Navbartest";
 //<Route exact path="lag" element={<Lag />} />
 
 function App() {
-  return (
-    <Router>
-      <NavbarNew />
-      <Routes>
-        <Route exact path="/" element={<StartPage />} />
-        <Route exact path="/domd.nu" element={<StartPage />} />
-        <Route exact path="/dimd" element={<DimD />} />
-        <Route exact path="/rules" element={<Rules />} />
-        <Route exact path="/start" element={<StartPage />} />
-      </Routes>
-    </Router>
-  );
+
+  
+  const domdDate = new Date("04/11/2024");
+  const currentDate = new Date();
+    
+  // To calculate the time difference of two dates
+  const differenceInTime = domdDate.getTime() - currentDate.getTime();
+    
+  // To calculate the no. of days between two dates
+  const daysUntilDomd = Math.floor(differenceInTime / (1000 * 3600 * 24));
+  console.log(daysUntilDomd)
+  
+  if (daysUntilDomd <= 100) {
+    return (
+      <Router>
+        <NavbarNew />
+        <Routes>
+          <Route exact path="/" element={<StartPage />} />
+          <Route exact path="/domd.nu" element={<StartPage />} />
+          <Route exact path="/dimd" element={<DimD />} />
+          <Route exact path="/rules" element={<Rules />} />
+          <Route exact path="/start" element={<StartPage />} />
+        </Routes>
+      </Router>
+    );
+  }
+  
+  return <Error404></Error404>
 }
 
 export default App;
