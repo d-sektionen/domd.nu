8fb2a272ea1b244ea2d11eea38993ccb246bfd37
Marc Taylor <marc@taylorit.com>
2024-01-16 23:30:31 +0100

    Fixed logics for dimd competition so it shows location and time

--- src/compoments/DomdCompetition/index.js
+++ src/compoments/DomdCompetition/index.js
@@ -10,31 +10,17 @@ import API_KEY from './config';
 //   }
 // })
 
+const SEMI_FINAL_BOARD1 = 2;
+const SEMI_FINAL_BOARD2 = 5;
+const FINAL_BOARD = 5;
+
 const TOURNAMENT_URL = 'https://api.challonge.com/v1/tournaments/testAvAPI/matches.json';
 
 const TOURNAMENT_ID = 'domd2023';
 // const TEAMS_URL = `https://api.challonge.com/v1/tournaments/${year}/participants.json`;
 
 
-
-const API_URL = 'https://jsonplaceholder.typicode.com/todos/1';
-
-// Function to fetch JSON data from the API
-async function testFetchFunc() {
-  try {
-    const response = await fetch(API_URL); // Fetch data from the API
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    const data = await response.json(); // Parse response body as JSON
-    console.log('Fetched data:', data); // Log the fetched data
-  } catch (error) {
-    console.error('There was a problem with the fetch operation:', error);
-  }
-}
-
 // const TOURNAMENT_ID = 'testAvAPI';
-const TEAM_ID = '192163634';
 const MATCHES_URL = `https://api.challonge.com/v1/tournaments/${TOURNAMENT_ID}/matches.json`;
 
 // Function to get the team's next game with time and station details
@@ -108,34 +94,144 @@ const MenuPropsTeam = {
   },
 };
 
+// Returns all games of a tournamentId
+async function fetchAllGames(tournamentId, apiKey) {
+  try {
+    const url = 'https://corsproxy.io/?' + encodeURIComponent(`https://api.challonge.com/v1/tournaments/${tournamentId}/matches.json?api_key=${apiKey}`);
+    const response = await fetch(url);    
+    
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    const matches = await response.json();
+    return matches;
+  } catch (error) {
+    console.error('Error fetching matches:', error);
+  }
+}
+
+// checks if a team has a game, return true or false
+function doesTeamHaveGame(allGames, teamId) {
+    const matches = allGames;
+    console.log("Matches json: ", matches);
+    // Check if teamId is in any match
+    const hasGame = matches.some(match => (match.match.player1_id == teamId || match.match.player2_id == teamId) && match.match.state !== 'complete');
+    console.log("Does player have game:", hasGame);
+    return hasGame;
+}
+
+// Returns the next game of a competing team
+function getNextGame(allGames, teamId) {
+  const matches = allGames;
+  console.log("Matches json: ", matches);
+  // Check if teamId is in any match
+  const game = matches.find(match => (match.match.player1_id == teamId || match.match.player2_id == teamId) && match.match.state !== 'complete');
+  console.log("This is the next game:", game);
+  return game;
+}
+
+// Return the dartBoardNumb of a certain game
+function getDartBoardNumb(tournament, allGames, game) {
+  if (tournament.toLowerCase().includes('dimd')) {
+    console.log(allGames.length, "amount of games")
+    // return other 
+    console.log(game.match.suggested_play_order, "game numb");
+    if(allGames.length - game.match.suggested_play_order <= 2) {
+      switch (game.match.suggested_play_order) {
+        case 61: 
+          return SEMI_FINAL_BOARD1;
+        case 62:
+          return SEMI_FINAL_BOARD2;
+        case 63:
+          return FINAL_BOARD;
+      }
+    } else {
+      return game.match.suggested_play_order % 12;
+    }
+  }
+  else if (tournament.toLowerCase().includes('domd')) {
+    if(allGames.length - game.match.suggested_play_order <= 6) {
+      switch (game.match.suggested_play_order) {
+        case 61: 
+          return SEMI_FINAL_BOARD1;
+        case 62:
+          return SEMI_FINAL_BOARD2;
+        case 63:
+          return FINAL_BOARD;
+        default:
+          return FINAL_BOARD;
+      }
+    } else {
+      return game.match.suggested_play_order % 32;
+    }
+  }
+}
+
 export default function DomdComp() {
   const [teams, setTeams] = useState([]);
-  const [year, setYear] = useState('');
-  const [selectedTeam, setSelectTeam] = useState('');
+  const [selectedTeam, setSelectedTeam] = useState('');
   const [displayTeam, setDisplayTeam] = useState(false);
+  const [teamHasGame, setTeamHasGame] = useState(false);
   const [tournamentID, setTournamentID] = useState('');
-  let TEAMS_URL = `https://api.challonge.com/v1/tournaments/${year}/participants.json`;
+  const [teamsID, setTeamsID] = useState({});
+  const [allGamesOfTournament, setAllGames] = useState([]);
+  const [time, setGameTime] = useState('');
+  const [boardNumb, setBoardNumb] = useState('');
+  const [location, setLocation] = useState('');
 
-  const changeYear = (event) => {
-    setYear(event.target.value);
-    setTournamentID(event)
-    
-    console.log(year);
+  const changeYear = async (event) => {
+    const tournamentName = event.target.value;
+    setTournamentID(tournamentName);
+    setAllGames(await fetchAllGames(tournamentName, API_KEY));
   }
 
   const changeTeam = (event) => {
-    setSelectTeam(event.target.value);
-    if (year !== '') {
+    const teamName = event.target.value;
+    setSelectedTeam(teamName);
+    if (teamName !== '') {
       setDisplayTeam(true);
+      console.log("got in here")
     }
-    console.log(event.target.value);
+
+    console.log(allGamesOfTournament, "all games");
+    // check if team has a game to play
+    const teamHasGame = doesTeamHaveGame(allGamesOfTournament, teamsID[teamName]);
+    if (teamHasGame) {
+      setTeamHasGame(true);
+      // TODO: update the time, boardNumb and location
+      const nextGame = getNextGame(allGamesOfTournament, teamsID[teamName]);
+      const gameTime = nextGame.match.scheduled_time;
+      const formattedTime = new Date(gameTime).toLocaleTimeString('sv-SE', {
+        hour: 'numeric',
+        minute: 'numeric',
+        hour12: false // If you want 24-hour format
+      });
+      setGameTime(formattedTime);
+
+      // checks if it not the last three games
+
+      const dartBoardNumb = getDartBoardNumb(tournamentID, allGamesOfTournament, nextGame);
+      setBoardNumb(dartBoardNumb);
+
+      if (dartBoardNumb <= 8) {
+        setLocation('Baljan');
+      } else if (dartBoardNumb <= 12) {
+        setLocation('Nano');
+      } else {
+        setLocation('Matsalen');
+      }
+    } else {
+      setTeamHasGame(false);
+      console.log("team does not have game");
+    }
   }
 
-    // Function to fetch all teams of a tournament
+  // Function to fetch all teams of a tournament, is updated as choosen competition is changed
   useEffect(() => {
     async function getAllTeams() {
-      console.log(year, 'fetching teams');
-      const url = 'https://corsproxy.io/?' + encodeURIComponent(`https://cors-anywhere.herokuapp.com/https://api.challonge.com/v1/tournaments/${year}/participants.json?api_key=${API_KEY}`); 
+      console.log(tournamentID, 'fetching teams');
+      const url = 'https://corsproxy.io/?' + encodeURIComponent(`https://cors-anywhere.herokuapp.com/https://api.challonge.com/v1/tournaments/${tournamentID}/participants.json?api_key=${API_KEY}`); 
       try {
         const response = await fetch(url);
         if (!response.ok) {
@@ -143,13 +239,19 @@ export default function DomdComp() {
         }
 
         const teams = await response.json();
-        console.log(teams);
-        let teamList = []
+        // console.log(teams);
+        let teamList = [];
+        let teamsIdentification = {};
         for (let team of teams) {
           teamList.push(team.participant.name);
-          console.log(team.participant.name);
+          // console.log(team.participant, "team info");
+          teamsIdentification[team.participant.name] = team.participant.id;
+          // console.log(team.participant.id);
         }
+        teamList.sort();
         setTeams(teamList);
+        setTeamsID(teamsIdentification);
+        console.log(teamsIdentification);
         // console.log('All teams of the tournament:', teams[0].participant.name);
       } catch (error) {
         console.error('Error fetching teams:', error);
@@ -157,9 +259,6 @@ export default function DomdComp() {
     }
     getAllTeams();
   }, [tournamentID]);
-
-  // Call the function to fetch all teams of the tournament
-  // getAllTeams();
 
   return (
 
@@ -180,7 +279,7 @@ export default function DomdComp() {
             autoWidth
             labelId="comp-select-label"
             id="comp-select"
-            value={year}
+            value={tournamentID}
             label="comp"
             onChange={changeYear}
             MenuProps={MenuPropsTeam}
@@ -191,6 +290,7 @@ export default function DomdComp() {
             // }}
           >
             {/* <MenuItem value={'dimd2024'} sx={{borderWidth:1}}>DIMD 2024</MenuItem> */}
+            <MenuItem value={'dimd2024'}>DIMD 2024</MenuItem>
             <MenuItem value={'domd2023'}>DÖMD 2023</MenuItem>
             <MenuItem value={'dimd2023'}>DIMD 2023</MenuItem>
           </Select>
@@ -254,21 +354,26 @@ export default function DomdComp() {
         px: '10px',}}
       >
         <Typography variant="h4" sx={{textAlign: 'center', display: displayTeam ? 'none' : 'block',}}>Ta reda på när ditt lag spelar under tävlingen!</Typography>
-        {/* <Typography
-          variant="h4"
-          sx={{
-            textAlign: 'center',
-            display: displayTeam ? 'block' : 'none',}}
-        >Lag {selectedTeam} spelar {time} i {location} på tavla {boardNumb}
-        </Typography> */}
         <Typography
           variant="h4"
           sx={{
             textAlign: 'center',
-            display: displayTeam ? 'block' : 'none',}}
-        >Lag {selectedTeam} har inga matcher att spela!
+            display: displayTeam && teamHasGame ? 'block' : 'none',}}
+        >Lag/Spelare {selectedTeam} spelar {time} i {location} på tavla {boardNumb}
+        </Typography>
+        <Typography
+          variant="h4"
+          sx={{
+            textAlign: 'center',
+            display: (displayTeam && !teamHasGame) ? 'block' : 'none',}}
+        >Lag/Spelare {selectedTeam} har inga matcher att spela!
         </Typography>
       </Box>
     </Box>
   );
 }
+
+
+// scheduled_time: "2024-02-07T10:30:00.000-06:00" == 17.30
+
+// scheduled_time: "2024-02-07T15:00:00.000-06:00" == 22.00
