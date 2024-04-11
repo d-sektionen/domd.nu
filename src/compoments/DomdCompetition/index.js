83fbeb94365cfc621b4fd7d7d213c7a4ec3a5d45
Marc Taylor <marc@taylorit.com>
2024-04-11 12:49:12 +0200

    Fixed sentence for final

--- src/compoments/DomdCompetition/index.js
+++ src/compoments/DomdCompetition/index.js
@@ -270,6 +270,8 @@ export default function DomdComp() {
   const [boardNumb, setBoardNumb] = useState('');
   const [location, setLocation] = useState('');
   const [loading, setLoading] = useState(false);  // State to manage loading indicator
+  const [isFinal, setIsFinal] = useState(false);  // State to manage loading indicator
+
 
   const changeTournament = async (event) => {
     const tournamentName = event.target.value;
@@ -301,6 +303,12 @@ export default function DomdComp() {
       });
 
       const dartBoardNumb = await getDartBoardNumb(tournamentID, allGamesOfTournament, nextGame);
+      if (dartBoardNumb == 'Finsittningen') {
+        setIsFinal(true)
+      } else {
+        setIsFinal(false)
+      }
+      
       setGameTime(formattedTime);
       setBoardNumb(dartBoardNumb);
 
@@ -436,8 +444,15 @@ export default function DomdComp() {
             variant="h4"
             sx={{
               textAlign: 'center',
-              display: displayTeam && teamHasGame ? 'block' : 'none',}}
+              display: displayTeam && teamHasGame && !isFinal ? 'block' : 'none',}}
               >Lag {selectedTeam} spelar {time} i {location} på tavla {boardNumb}
+          </Typography>
+          <Typography
+            variant="h4"
+            sx={{
+              textAlign: 'center',
+              display: displayTeam && teamHasGame && isFinal ? 'block' : 'none',}}
+              >Lag {selectedTeam} spelar {time} i matsalen på {boardNumb}
           </Typography>
           <Typography
             variant="h4"
